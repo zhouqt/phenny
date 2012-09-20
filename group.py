e594239954608d01204f5a96c9b017ac269a985d
Qingtang Zhou <qzhou@redhat.com>
2012-09-20 19:47:20 +0800

    phenny: Add user group support for this bot
    This patch adds user group support for phenny, it can manage groups
    of user per channel.
    
    There is a module called group_cmd, provide 2 cmds for user group
    management task:
    
     * .group list/add/del/save/load @groupname
     * .member list/add/del/save @groupname nick nick nick...
    
    Signed-off-by: Qingtang Zhou <qzhou@redhat.com>

---
+++ group.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+"""
+group.py - User Group Facilities
+
+Author: Qingtang Zhou <qzhou@redhat.com>
+About: http://inamidst.com/phenny/
+"""
+
+import os
+import cPickle
+
+
+class GroupManager(object):
+    def __init__(self, phenny, channel):
+        self.phenny = phenny
+        self.channel = channel
+        fn = "%s-%s-%s.group.db" % (self.phenny.config.nick, self.channel,
+                                    self.phenny.config.host)
+        self.db_filename = os.path.join(os.path.expanduser('~/.phenny'), fn)
+        self.group_list = []
+        self.load_group_list()
+
+    def load_group_list(self, filename=None):
+        fn = filename or self.db_filename
+        try:
+            f = open(fn, 'r')
+            self.group_list = cPickle.load(f)
+            f.close()
+            return "Okay"
+        except Exception, e:
+            return "Error when loading group info: %s" % e
+
+
+    def save_group_list(self, filename=None):
+        fn = filename or self.db_filename
+        f = open(fn, 'w')
+        cPickle.dump(self.group_list, f)
+        f.close()
+        return "Okay"
+
+    def get_group(self, group_name):
+        grp = [grp for grp in self.group_list if (grp.name == group_name)]
+        if grp:
+            return grp[0]
+        return None
+
+    def has_group(self, group_name):
+        if self.get_group(group_name):
+            return True
+        return False
+
+    def add_group(self, name, creator, members=[]):
+        if not name:
+            return ("Incorrect group name. "
+                    "Please add a '@' in front of group name")
+        if self.has_group(name):
+            return "There is alreay a group called '%s'" % name
+        grp = Group(name, creator, members)
+        self.group_list.append(grp)
+        self.save_group_list()
+        return "Okay"
+
+    def del_group(self, name, user, members=[]):
+        grp = self.get_group(name)
+        if not grp:
+            return "No such group '%s'" % name
+        if user != grp.creator:
+            return ("You're not the group creator,"
+                   " ask group creator '%s' for help" % grp.creator)
+        self.group_list.remove(grp)
+        self.save_group_list()
+        return "Okay"
+
+
+class Group(object):
+    def __init__(self, name, creator, members=[]):
+        self.name = name
+        self.creator = creator
+        self.members = members
+
+    def lookup_member(self, member):
+        if member in self.members:
+            return True
+        return False
+
+    def add_member(self, member):
+        if self.lookup_member(member):
+            return False
+
+        self.members.append(member)
+        return True
+
+    def del_member(self, member):
+        if not self.lookup_member(member):
+            return False
+
+        self.members.remove(member)
+        return True
+
+
+if __name__ == "__main__":
+    pass
---
+++ modules/group_cmd.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+"""
+startup.py - Phenny Group Module
+
+Author: Qingtang Zhou <qzhou@redhat.com>
+"""
+
+import sys
+import re
+import group
+
+def setup(phenny):
+    phenny.group_managers = {}
+    for c in phenny.channels:
+        phenny.group_managers[c] = group.GroupManager(phenny, c)
+
+
+def group_cmd(phenny, input_msg):
+    usage = "Usage: .group list/add/del/load/save @groupname"
+
+    def list_grp():
+        if gm.group_list:
+            grp =  "@" + " @".join([g.name for g in gm.group_list])
+        else:
+            grp = "(N/A)"
+        return "Existed groups: %s" % grp
+
+    gm = phenny.group_managers.get(input_msg.sender)
+    if not gm:
+        print >> sys.stderr, ("GroupManager init failed for"
+                             " channel '%s'" % input_msg.sender)
+        return phenny.reply("OOPS!")
+
+    args = input_msg.group(2)
+    if not args:
+        phenny.reply(usage)
+        return phenny.reply(list_grp())
+
+    print >> sys.stderr, input_msg.bytes
+    regex_str = r"\.group\s+(\w{3,4})(?:\s+@(\w+)|(?:.*))"
+    try:
+        cmd, name = re.findall(regex_str, input_msg.bytes)[0]
+    except Exception, e:
+        print >> sys.stderr, "Err: %s (in group_cmd.py)" % e
+        return phenny.reply(usage)
+
+    cmd_dict = {}
+    cmd_dict["list"] = lambda x,y: list_grp()
+    cmd_dict["add"] = gm.add_group
+    cmd_dict["del"] = gm.del_group
+    cmd_dict["load"] = lambda x,y: gm.load_group_list()
+    cmd_dict["save"] = lambda x,y: gm.save_group_list()
+
+    try:
+        if callable(cmd_dict[cmd]):
+            ret = cmd_dict[cmd](name, input_msg.nick.lower())
+            return phenny.reply(ret)
+    except KeyError:
+        print >> sys.stderr, "Can't find function for %s" % cmd
+        return phenny.reply("OOPS!")
+
+group_cmd.commands = ["group"]
+group_cmd.example = ".group list/add/del/load/save @groupname"
+group_cmd.priority = "low"
+
+
+def member_cmd(phenny, input_msg):
+    usage = "Usage: .member list/add/del/save @groupname nick nick nick"
+
+    def grp_member_info(grp):
+        m =  " ".join(grp.members)
+        if not m:
+            m = "(N/A)"
+        return ("Group @%s is created by %s,"
+                " nicks in this groups: %s" % (grp.name, grp.creator, m))
+
+    def grp_member_add(grp, user, nicks):
+        if user != grp.creator:
+            msg = "You're not the creator of this group!"
+            # User can add himself to a group.
+            if user in nicks:
+                msg += " You can only add yourself to it."
+                grp.add_member(user)
+                gm.save_group_list()
+            return msg
+
+        nicks = nicks.replace(" ", ",").split(",")
+        existed_member = []
+        for n in nicks:
+            if not grp.add_member(n):
+                existed_member.append(n)
+
+        # Call GroupManager to save new group info.
+        gm.save_group_list()
+        if existed_member:
+            return "%s already in this group." % " ".join(existed_member)
+        return "Okay"
+
+    def grp_member_del(grp, user, nicks):
+        if user != grp.creator:
+            msg = "You're not the creator of this group!"
+            # User can del himself from a group.
+            if user in nicks:
+                msg += " You can only remove yourself from it."
+                grp.del_member(user)
+                gm.save_group_list()
+            return msg
+
+        nicks = nicks.replace(" ", ",").split(",")
+        for n in nicks:
+            grp.del_member(n)
+
+        # Call GroupManager to save new group info.
+        gm.save_group_list()
+        return "Okay"
+
+    gm = phenny.group_managers.get(input_msg.sender)
+    if not gm:
+        print >> sys.stderr, ("GroupManager init failed for"
+                             " channel '%s'" % input_msg.sender)
+        return phenny.reply("OOPS!")
+
+    args = input_msg.group(2)
+    if not args:
+        return phenny.reply(usage)
+
+    print >> sys.stderr, input_msg.bytes
+    regex_str = r"\.member\s+(\w{3,4})\s+@(\w+)\s*(.*)"
+    try:
+        cmd, name, nicks = re.findall(regex_str, input_msg.bytes)[0]
+    except Exception, e:
+        print >> sys.stderr, "Err: %s (in group_cmd.py)" % e
+        return phenny.reply(usage)
+
+    grp = gm.get_group(name)
+    if not grp:
+        return phenny.reply("No such group '%s'" % name)
+
+    cmd_dict = {}
+    cmd_dict["list"] = lambda x,y,z: grp_member_info(x)
+    cmd_dict["add"] = grp_member_add
+    cmd_dict["del"] = grp_member_del
+    cmd_dict["save"] = lambda x,y,z: gm.save_group_list()
+
+    try:
+        if callable(cmd_dict[cmd]):
+            ret = cmd_dict[cmd](grp, input_msg.nick.lower(), nicks)
+            return phenny.reply(ret)
+    except KeyError:
+        print >> sys.stderr, "Can't find function for %s" % cmd
+        return phenny.reply("OOPS!")
+member_cmd.commands = ["member"]
+member_cmd.example = ".member list/add/del/save @groupname nick nick nick"
+member_cmd.priority = "low"
+
+if __name__ == '__main__':
+   print __doc__.strip()
