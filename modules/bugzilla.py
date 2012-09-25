8cfe51d0ce2da5fdab1bdccc2a2360fd29a331a8
Qingtang Zhou <qzhou@redhat.com>
2012-09-25 13:32:16 +0800

    Module.bugzilla: Query bug detail info
    Add a new cmd for querying bug info when input
    bugzilla url or bug id.
    
    Signed-off-by: Qingtang Zhou <qzhou@redhat.com>

--- modules/bugzilla.py
+++ modules/bugzilla.py
@@ -16,15 +16,36 @@ Remember to use comma ',' when status/component/product .
 eg. query all NEW and ASSIGNED bugs in qemu-kvm component of product 'Red Hat Enterprise Linux 6',
 type: '.bz NEW qemu-kvm rhel6'"""
 
-def bz(phenny, input_msg):
+BZ_FORMAT_STR = "#%{bug_id}: %{status} - %{assigned_to} - %{short_desc} [%{target_milestone}] [%{flags}] %{qa_whiteboard}"
+
+def get_bz_tool_path(phenny):
     if hasattr(phenny.config, 'bugzilla_tool_path'):
-         bz_tool_path = phenny.config.bugzilla_tool_path
-    else:
-        phenny.say("Missing bugzilla tools, add 'bugzilla_tool_path'"
-                   " in my config file.")
+         return phenny.config.bugzilla_tool_path
+
+    phenny.say("Missing bugzilla tools, add 'bugzilla_tool_path'"
+               " in my config file.")
+    return ""
+
+
+def query_bug_with_id(phenny, bugid):
+    cmd = get_bz_tool_path(phenny)
+    if not cmd:
+        return
+    cmd += " query"
+    cmd += " --outputformat '%s'" % BZ_FORMAT_STR
+    cmd += " -b %s" % bugid
+    print cmd
+    s, out = commands.getstatusoutput(cmd)
+    if s:
+        return phenny.say("Failed to call bugzilla cli tool!")
+    return phenny.say(out)
+
+
+def bz(phenny, input_msg):
+    cmd = get_bz_tool_path(phenny)
+    if not cmd:
         return
 
-    cmd = bz_tool_path
     args = input_msg.group(2)
     if not args:
         return phenny.reply(usage)
@@ -32,17 +53,19 @@ def bz(phenny, input_msg):
     args = args.strip()
 
     if args.isdigit():
-        cmd += " query -b %s" % args
-    else:
-        args = args.split()
-        if len(args) < 3:
-            return phenny.reply(usage)
-        status, component, product = args
-        if product.lower() == "rhel6":
-            product = "Red Hat Enterprise Linux 6"
-        elif product.lower() == "rhel5":
-            product = "Red Hat Enterprise Linux 5"
-        cmd += " query -t %s -c %s -p '%s'" % (status, component, product)
+        query_bug_with_id(phenny, args)
+        return
+
+    # query component.
+    args = args.split()
+    if len(args) < 3:
+        return phenny.reply(usage)
+    status, component, product = args
+    if product.lower() == "rhel6":
+        product = "Red Hat Enterprise Linux 6"
+    elif product.lower() == "rhel5":
+        product = "Red Hat Enterprise Linux 5"
+    cmd += " query -t %s -c %s -p '%s'" % (status, component, product)
     s, out = commands.getstatusoutput(cmd)
     if s:
         return phenny.say("Failed to call bugzilla cli tool!")
@@ -53,7 +76,6 @@ def bz(phenny, input_msg):
         return
     else:
         return phenny.reply(out)
-
 bz.commands = ['bz']
 bz.name = 'bz'
 bz.example = '.bz bzid/ <status> <component> <product>'
@@ -64,24 +86,24 @@ def bz_info(phenny, input_msg):
     if not input_msg.sender.startswith('#'):
         return
 
-    if hasattr(phenny.config, 'bugzilla_tool_path'):
-         bz_tool_path = phenny.config.bugzilla_tool_path
-    else:
-        phenny.say("Missing bugzilla tools, add 'bugzilla_tool_path'"
-                   " in my config file.")
+    cmd = get_bz_tool_path(phenny)
+    if not cmd:
         return
 
-    args = input_msg.group(2)
-    cmd = bz_tool_path
-    if args.isdigit():
-        cmd += " query -b %s" % args
+    args = input_msg.group(1)
+    query_bug_with_id(phenny, args)
+    return
+bz_info.rule = r'.*?https:\/\/bugzilla\.redhat\.com\/.*?(\d{1,6}).*'
+bz_info.priority = 'high'
 
 def bz_url(phenny, input_msg):
     if not input_msg.sender.startswith('#'):
         return
 
     arg = input_msg.group(1)
-    return phenny.reply("Bugzilla URL: %s" % os.path.join(BUGZILLA_URL, arg))
+    phenny.reply("Bugzilla URL: %s" % os.path.join(BUGZILLA_URL, arg))
+    query_bug_with_id(phenny, arg)
+    return
 bz_url.rule = r'.*?[Bb](?:[Uu][Gg]|[Zz])[: ,#]*(\d{1,6}).*'
 bz_url.priority = 'high'
 
